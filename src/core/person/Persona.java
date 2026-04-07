3f28c951b5984662222f841ae801a78936e49b72
g4bblcs <glcampo@uninorte.edu.co>
2026-04-07 12:55:00 -0500

    clases

---
+++ src/core/person/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.person;
+
+/**
+ *
+ * @author Gabriela Campo
+ */
+public abstract class Persona {
+    protected String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
---
+++ src/core/radio/Genero.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.radio;
+
+/**
+ *
+ * @author Gabriela Campo
+ */
+public enum Genero {
+    ROCK,
+    CLASICA,
+    TROPICAL
+}
